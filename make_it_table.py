21da9fd58f4227871c2fcdde407bce29167972e3
RobinLoksli92 <gleb11tiun@mail.ru>
2021-12-29 13:36:06 +0200

    pep redact

--- make_it_table.py
+++ make_it_table.py
@@ -1,13 +1,17 @@
 def make_it_table(salaries_dict):
-    table_data = [['Язык программирования', 'Вакансий найдено', 'Вакансий обработано', 'Средняя зарплата']]
+    table_data = [
+        ['Язык программирования',
+         'Вакансий найдено',
+         'Вакансий обработано',
+         'Средняя зарплата']
+    ]
     for developer_type, salary_info in salaries_dict.items():
-        table_data.append([developer_type, salary_info['vacancies_found'], salary_info['vacancies_processed'], salary_info['average_salary']])   
+        table_data.append(
+            [developer_type,
+             salary_info['vacancies_found'],
+             salary_info['vacancies_processed'],
+             salary_info['average_salary']]
+        )
     return table_data
 
 
-def main():
-    pass
-
-
-if __name__ == '__main__':
-    main()
