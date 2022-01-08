73068d626a69844430b49009ae5e02d1a4b6d020
RobinLoksli92 <gleb11tiun@mail.ru>
2022-01-08 17:38:37 +0200

    Убраны типы данных в названии в make_it_table.py

--- make_it_table.py
+++ make_it_table.py
@@ -1,14 +1,14 @@
 from terminaltables import AsciiTable
 
 
-def make_it_table(salaries_dict, title):
+def make_it_table(salaries, title):
     table_rows = [
         ['Язык программирования',
          'Вакансий найдено',
          'Вакансий обработано',
          'Средняя зарплата']
     ]
-    for developer_type, salary_details in salaries_dict.items():
+    for developer_type, salary_details in salaries.items():
         table_rows.append(
             [developer_type,
              salary_details['vacancies_found'],
